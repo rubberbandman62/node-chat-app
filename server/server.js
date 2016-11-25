843daa47da7eb77ea0f9dfbf306a7d67c40d7b7e
Tödter <hto@iks-gmbh.com>
2016-11-25 17:36:16 +0100

    Add welcome message for new users.

--- server/server.js
+++ server/server.js
@@ -14,13 +14,25 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log("user connected!");
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to Chat App!',
+        createdAt: new Date().toTimeString()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined!',
+        createdAt: new Date().toTimeString()
+    });
 
     socket.on('createMessage', (message) => {
+        console.log('New user connected!');
+
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().toTimeString()
-        })
+        });
     });
 
     socket.on('disconnect', () => {
